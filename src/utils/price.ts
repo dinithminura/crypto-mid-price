2bd297a1eda6398746d9020b9fa7f05c54475ad2
Dinith Minura <dinithminura@gmail.com>
2024-04-01 23:08:34 +0800

    Added an aditional null check to orderbook

--- src/utils/price.ts
+++ src/utils/price.ts
@@ -8,7 +8,13 @@ import { OrderBookData } from "../global/types";
  * @returns The calculated mid-price.
  */
 export const calculateMidPrice = (bids: [string, string][], asks: [string, string][]): number => {
-    
+    if (!bids || bids.length === 0 || !bids[0] || !bids[0][0]) {
+        throw new Error('Invalid or missing bid data');
+    }
+
+    if (!asks || asks.length === 0 || !asks[0] || !asks[0][0]) {
+        throw new Error('Invalid or missing ask data');
+    }
     // Parse the best bid and ask prices from the order book, always comes in 0 index
     const bestBid = parseFloat(bids[0][0]);
     const bestAsk = parseFloat(asks[0][0]);
@@ -23,8 +29,11 @@ export const calculateMidPrice = (bids: [string, string][], asks: [string, strin
  * @returns The average mid-price calculated from the provided order books.
  */
 export const calculateAverageMidPrice = (orderBooks: OrderBookData[]): number => {
+    if (orderBooks.length === 0) {
+        throw new Error('No order book data provided');
+    }
+
     let totalMidPrice = 0;
-    // check null in loop -> throw error 
     for (const orderBook of orderBooks) {        
         if (!orderBook || !orderBook.bids || !orderBook.asks) {
             throw new Error('Invalid or missing order book data');
