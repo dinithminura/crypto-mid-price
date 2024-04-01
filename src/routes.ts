a4778034d2197a5a41b0f11214c014851b660127
Dinith Minura <dinithminura@gmail.com>
2024-04-01 16:47:45 +0800

    added api health check endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import { getGlobalPriceIndex } from './controllers/globalPriceController';
+import { healthCheck } from './controllers/healthCheckController';
 
 const router = express.Router();
+
+// Health check endpoint
+router.get('/health-check', healthCheck);
 
 // Define route to get global price index
 router.get('/global-price-index', getGlobalPriceIndex);
